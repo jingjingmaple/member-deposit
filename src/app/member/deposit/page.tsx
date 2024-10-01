444da5216d8eea3cf30d7d00e48ab026f9105268
Sorachat Patanasombat <sorachat.p@haupcar.com>
2024-10-01 21:02:32 +0700

    Create make deposit page

---
+++ src/app/member/deposit/page.tsx
@@ -0,0 +1,103 @@
+'use client'
+
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { useState } from 'react'
+const amountChoice = [
+  {
+    id: 1,
+    name: '100 THB',
+  },
+  {
+    id: 2,
+    name: '500 THB',
+  },
+  {
+    id: 3,
+    name: '1000 THB',
+  },
+]
+export default function Example() {
+  const [agreed, setAgreed] = useState(false)
+  const [selected, setSelected] = useState(amountChoice[0])
+
+  return (
+    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+        />
+      </div>
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Make a deposit
+        </h2>
+        <p className="mt-2 text-lg leading-8 text-gray-600">Please select your desired amount</p>
+      </div>
+      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="company"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Amount
+            </label>
+            <div className="mt-2.5">
+              <Listbox value={selected} onChange={setSelected}>
+                <div className="relative mt-2">
+                  <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                    <span className="flex items-center">
+                      <span className="ml-3 block truncate">{selected.name}</span>
+                    </span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                      <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
+                    </span>
+                  </ListboxButton>
+
+                  <ListboxOptions
+                    transition
+                    className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
+                  >
+                    {amountChoice.map((item) => (
+                      <ListboxOption
+                        key={item.id}
+                        value={item}
+                        className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+                      >
+                        <div className="flex items-center">
+                          <span className="ml-3 block truncate font-normal group-data-[selected]:font-semibold">
+                            {item.name}
+                          </span>
+                        </div>
+
+                        <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                          <CheckIcon aria-hidden="true" className="h-5 w-5" />
+                        </span>
+                      </ListboxOption>
+                    ))}
+                  </ListboxOptions>
+                </div>
+              </Listbox>
+            </div>
+          </div>
+        </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Proceed
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
